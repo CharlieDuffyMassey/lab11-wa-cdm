92dd3f484cb80e909c96c40f5ed312f2e6a67758
charlesduffy-massey <charlieduffymassey@gmail.com>
2025-11-12 18:15:35 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -21,27 +21,11 @@ def hypotenuse(a, b):
 
    # math.hypot(a,b)
 # First example
-def add(a, b):
-    return a + b
-
-def sub (a , b):
-    return a - b
-
-def mul(a, b):
-    return a * b
 
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b/a
-
-def log(a,b):
-    if b < 0:
-        raise ValueError
-    return math.log(b, a)
-
-def exp( a, b):
-    return a ** b
 
 import math
 
