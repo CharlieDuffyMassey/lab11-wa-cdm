5911ce8b6e081154f7ff42123b8fb5583a3285f0
starwing07 <harveywillc@gmail.com>
2025-11-12 19:08:59 -0500

    final calculator commit

--- calculator.py
+++ calculator.py
@@ -6,30 +6,26 @@ calculator.py
 
 One function per operation, in order.
 """
+def square_root(a):
+    # if a < 0:
+    #     raise ValueError
+    try:
+        if a < 0:
+            raise ValueError
+        return math.sqrt(a)
+    except ValueError as b:
+            print(b)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+   # math.hypot(a,b)
 # First example
-def add(a, b):
-    return a + b
-
-def sub (a , b):
-    return a - b
-
-def mul(a, b):
-    return a * b
 
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b/a
-
-def log(a,b):
-    if b < 0:
-        raise ValueError
-    return math.log(b, a)
-
-def exp( a, b):
-    return a ** b
-
-import math
 
 def add(a, b):
     return a + b
@@ -43,6 +39,6 @@ def mul(a, b):
 def log(a, b):
     if a <= 0 or a ==  1 or b <= 0:
         raise ValueError("Invalid value(s).")
-
+    return math.log(b, a)
 def exp(a, b):
     return a ** b
